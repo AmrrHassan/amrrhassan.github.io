85f5f5fb347819d18d96a6bb8e28a7d0eb0a3324
amrrhassan <amrhassan98@hotmail.com>
2024-09-11 19:42:01 +0300

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,29 +2,39 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
-  export const metadata: Metadata = {
-    title: "Amr Hassan",
-    description: "Full-Stack Engineer / Developer Portfolio",
-    openGraph: {
-      title: "Amr Hassan - Full-Stack Engineer",
-      description: "Explore Amr Hassan's portfolio, showcasing full-stack development projects and expertise.",
-      url: "https://amrrhassan.github.io", 
-      type: "website",
-      images: [
-        {
-          url: "https://amrrhassan.github.io/favicon-32x32.png",
-          width: 1200,
-          height: 630,
-          alt: "Amr Hassan Portfolio",
-        },
-      ],
-    },
-    keywords: ["Full-Stack Engineer", "Web Developer", "Amr Hassan Portfolio", "React", "Node.js", "WordPress", "Amr Hafez", "Amr Hassan Hafez"],
-    robots: "index, follow",
-  };
-  
+export const metadata: Metadata = {
+  title: "Amr Hassan",
+  description: "Full-Stack Engineer / Developer Portfolio",
+  openGraph: {
+    title: "Amr Hassan - Full-Stack Engineer",
+    description:
+      "Explore Amr Hassan's portfolio, showcasing full-stack development projects and expertise.",
+    url: "https://amrrhassan.github.io",
+    type: "website",
+    images: [
+      {
+        url: "https://amrrhassan.github.io/favicon-32x32.png",
+        width: 1200,
+        height: 630,
+        alt: "Amr Hassan Portfolio",
+      },
+    ],
+  },
+  keywords: [
+    "Full-Stack Engineer",
+    "Web Developer",
+    "Amr Hassan Portfolio",
+    "React",
+    "Node.js",
+    "WordPress",
+    "Amr Hafez",
+    "Amr Hassan Hafez",
+  ],
+  robots: "index, follow",
+};
 
 export default function RootLayout({
   children,
@@ -33,6 +43,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-T435H88EVQ`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('js', new Date());
+              gtag('config', 'G-T435H88EVQ');
+            `,
+          }}
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
